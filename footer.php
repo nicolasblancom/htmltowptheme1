67cccb45c014cbfd8008ee66a0b66f53ec1bb2c6
nicolasblancom <nicolasblancom@gmail.com>
2016-07-26 18:04:24 +0200

    adding data to header and footer

--- footer.php
+++ footer.php
@@ -1,1 +1,8 @@
-<p>footer here...</p>
+    <div class="footer-clear"></div>
+    <footer class="row no-max pad">
+        <p>Copyright <?php echo date('Y') ?></p>
+    </footer>
+
+    <?php wp_footer() ?>
+</body>
+</html>
