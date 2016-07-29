640a1f9cc622af12187839f58f92641791a5c054
nicolasblancom <nicolasblancom@gmail.com>
2016-07-29 17:11:13 +0200

    add hability to create menus from admin area, create some menus, display menu in header

--- functions.php
+++ functions.php
@@ -1,5 +1,17 @@
 <?php
 
+// adds menu option from admin area
+add_theme_support( 'menus' );
+
+// register some menus, later we can add a newly created menu to one of these
+function htmltowp_register_theme_menus(){
+    register_nav_menus(array(
+        'primary-menu' => __('Primary Menu')
+    ));
+}
+
+add_action( 'init', 'htmltowp_register_theme_menus' );
+
 // adds (enqueues) stylesheets to the theme
 function htmltowp_theme_styles(){
     wp_enqueue_style('foundation_css', get_template_directory_uri() . '/css/foundation.css');
--- header.php
+++ header.php
@@ -13,7 +13,18 @@
         <a href="" class="nav-toggle"><span></span>Menu</a>
         <nav>
             <h1 class="open"><a class='current' href="index.html">G</a></h1>
-            <ul class="no-bullet">
+
+            <?php
+            // sets menu default options, and prints out the 'primary menu' in this particular place
+            $defaults = array(
+                'container' => false,
+                'theme_location' => 'primary-menu',
+                'menu_class' => 'no-bullet'
+            );
+            wp_nav_menu( $defaults );
+            ?>
+
+            <!--<ul class="no-bullet">
                 <li class="current parent"><a class='current' href="<?php bloginfo('url'); ?>">Portfolio</a>
                     <ul class="sub-menu">
                         <li><a href="item.html">Portfolio Item</a></li>
@@ -30,6 +41,6 @@
                 </li>
                 <li><a href="about.html">About</a></li>
                 <li><a href="contact.html">Contact</a></li>
-            </ul>
+            </ul>-->
         </nav>
     </header>
