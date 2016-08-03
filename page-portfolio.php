1a616d73ace32e2842bbb15e7fa792cb842961f7
nicolasblancom <nicolasblancom@gmail.com>
2016-08-03 16:19:21 +0200

    extract partial for portfolio items

--- page-portfolio.php
+++ page-portfolio.php
@@ -20,27 +20,6 @@
     </div>
 </section>
 
-<?php
-// custom loop to pull a custom post type
-$args = array(
-    'post_type' => 'portfolio'
-);
-
-$query = new WP_Query( $args );
-?>
-
-<!-- porfolio-section -->
-<section class="row no-max pad">
-
-    <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
-
-    <div class="small-6 medium-4 large-3 columns grid-item">
-        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large'); ?></a>
-    </div>
-
-    <?php endwhile; endif; wp_reset_postdata(); ?>
-
-<!-- /porfolio-section -->
-</section>
+<?php get_template_part( 'content', 'portfolio' ); ?>
 
 <?php get_footer(); ?>
