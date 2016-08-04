b1aaa2d5330d9942ec660123f63a729ad6b293b0
nicolasblancom <nicolasblancom@gmail.com>
2016-08-04 16:48:49 +0200

    replace sidebar with widgets

--- archive.php
+++ archive.php
@@ -44,12 +44,8 @@
             </div>
 
 
-            <!-- Secondary Column -->
-            <div class="small-12 medium-4 medium-pull-8 columns">
-                <div class="secondary">
-                    <h2 class="module-heading">SIDEBAR</h2>
-                </div>
-            </div>
+            <?php get_sidebar(); ?>
+            
         </div>
     </div>
 </section>
